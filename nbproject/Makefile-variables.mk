103567018e344287f05db98cfd6023501a8d71f1
Tyler Weaver <tylerjw@gmail.com>
2013-12-21 10:57:13 -0700

    working demo of UART1

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SerialDemo.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SerialDemo.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=serialdemo.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/serialdemo.x.tar
